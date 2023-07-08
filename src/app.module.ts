c7b49bf78062e9421875c13761f245e593de8121
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2023-07-08 21:13:34 +0800

    env var validation, mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './modules/pokemon/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import Joi from 'joi';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'staging')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        MONGO_DB_URL: Joi.string(),
+      }),
+      validationOptions: {
+        allowUnknown: false,
+        abortEarly: true,
+      },
+    }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
+    PokemonModule,
+  ],
   controllers: [],
   providers: [],
 })
