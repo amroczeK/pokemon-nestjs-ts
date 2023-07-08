ace9c1fa84ce90a2c24192b53492fe88c6f2212a
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2023-07-08 21:08:30 +0800

    pokemon module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PokemonModule } from './modules/pokemon/pokemon.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PokemonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
