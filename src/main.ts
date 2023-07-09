cc830b5f877ab1af0ecda971f5e79b652b6015df
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2023-07-09 15:32:13 +0800

    implement api versioning

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Pokemon Gen 1')
--- src/modules/pokemon/pokemon.controller.ts
+++ src/modules/pokemon/pokemon.controller.ts
@@ -7,19 +7,20 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  Version,
 } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
 import { Pokemon } from './schemas/pokemon.schema';
-import { plainToClass } from 'class-transformer';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
   @Post()
+  @Version('1')
   create(
     @Body(new ValidationPipe()) createPokemonDto: CreatePokemonDto,
   ): Promise<Pokemon> {
@@ -27,16 +28,19 @@ export class PokemonController {
   }
 
   @Get()
+  @Version('1')
   findAll(): Promise<Pokemon[]> {
     return this.pokemonService.findAll();
   }
 
   @Get(':id')
+  @Version('1')
   findOne(@Param('id', new ParseIntPipe()) id: string): Promise<Pokemon> {
     return this.pokemonService.findOne(+id);
   }
 
   @Patch(':id')
+  @Version('1')
   update(
     @Param('id', new ParseIntPipe()) id: string,
     @Body(new ValidationPipe()) updatePokemonDto: UpdatePokemonDto,
@@ -45,6 +49,7 @@ export class PokemonController {
   }
 
   @Delete(':id')
+  @Version('1')
   remove(@Param('id', new ParseIntPipe()) id: string): Promise<void> {
     return this.pokemonService.remove(+id);
   }
